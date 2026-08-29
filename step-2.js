6142f3c401d9efa7e5770c63e73bba5cbdac309f
fatimasiitu <sagidullinafatima@gmail.com>
2026-08-29 14:06:14 +0500

    Delete step-2.js

--- step-2.js
+++
@@ -1,8 +0,0 @@
-const cards = document.querySelectorAll(".option-card");
-
-cards.forEach(card => {
-  card.addEventListener("click", () => {
-    cards.forEach(c => c.classList.remove("active"));
-    card.classList.add("active");
-  });
-});
